1cf7798812313129316702a39db6fcd673f80f4f
ArsenioDraco <voynovarseny@gmail.com>
2025-12-09 15:45:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,4 +19,28 @@ let showGrid = false;
         generateDrawing();
       });
     }
+ function generateDrawing() {
+      background(254, 252, 249);
+      if (showGrid) drawGrid();
+
+      let doodles = floor(random(6, 11));
+      for (let i = 0; i < doodles; i++) {
+        let x = random(width);
+        let y = random(height);
+        let s = random(0.6, 1.4);
+        push();
+        translate(x, y);
+        scale(s);
+        let type = floor(random(6));
+        switch (type) {
+          case 0: drawSun(); break;
+          case 1: drawHouse(); break;
+          case 2: drawStickman(); break;
+          case 3: drawFlower(); break;
+          case 4: drawTree(); break;
+          case 5: drawName(); break;
+        }
+        pop();
+      }
+    }
 
