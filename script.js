5ce37ff4edfcc199ba91d197225f65a0029638c9
ArsenioDraco <voynovarseny@gmail.com>
2025-12-09 10:44:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let showGrid = false;
+
+    function setup() {
+      createCanvas(800, 600);
+      noLoop();
+      generateDrawing();
+
+      document.getElementById('newDrawing').addEventListener('click', () => {
+        clear();
+        generateDrawing();
+      });
 
